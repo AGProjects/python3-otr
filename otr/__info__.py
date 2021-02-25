29e0688d82ee731f8e27b26eebefee1d652421be
Adrian Georgescu <ag@ag-projects.com>
2021-02-25 11:13:23 +0000

    Added version 2.0.1

--- otr/__info__.py
+++ otr/__info__.py
@@ -5,7 +5,7 @@ __project__ = "python3-otr"
 __summary__ = "Off-The-Record Messaging (OTR) protocol implementation for python"
 __webpage__ = "https://github.com/AGProjects/python3-otr"
 
-__version__ = "3.0.0"
+__version__ = "2.0.1"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
