0a950d29b91daa98c30ab93bb93fb87d2831e2a5
Tijmen de Mes <tijmen@ag-projects.com>
2024-12-20 13:35:20 +0000

    Updated changelog and raised version

--- otr/__info__.py
+++ otr/__info__.py
@@ -5,7 +5,7 @@ __project__ = "python3-otr"
 __summary__ = "Off-The-Record Messaging (OTR) protocol implementation for python"
 __webpage__ = "https://github.com/AGProjects/python3-otr"
 
-__version__ = "2.0.2"
+__version__ = "2.1"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
